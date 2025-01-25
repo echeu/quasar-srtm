ecdb59025cc594aae3f1c2f85767ce3766ee9604
Elliott Cheu <echeu@arizona.edu>
2025-01-25 16:25:19 -0700

    Get latest sensor data

--- Device/src/DRegs.cpp
+++ Device/src/DRegs.cpp
@@ -60,146 +60,6 @@
 #include "json_provider.h"
 
 
-/* These are the i2C sensor counts. */
-#define NTEMP 1      /* Temperatures from tmp100 */
-#define NPOWER 9    /* VI ltc2945 */
-#define NSTATUS 1    /* PCA power good register */
-#define NFFFEX 4     /* 12 channel TX only modules */
-#define NFFFELIX 2   /* 4 channel TX/RX modules */
-#define NFIREFLY (NFFFEX+NFFFELIX)
-#define NPSEQ 1
-#define NDDR4 1
-#define NZYNQ 1  /* For the internal Zynq sensors */
-
-#define nFFchan 4
-
-
-/* --- SRTM sensors -----------------------------------------------------------
- * First, we do the devices on the RTM_SENSOR bus. This is shared with the
- * Zone 3 I2C from the LASP/SRTM tester card.
- *
- *  12 V DC-DC temperature
- */
-#define TMP100A  0x4F /* U2 */
-
-/* This is an ltc2945, but it's connected to the Zynq temperature diode,
-   so it's really a temperature sensor. Does this need special handling?
- */
-#define TMPZYNQ 0x6A
-
-/* This is the PCA8575 register with the power status bits */
-#define PSTATUS 0x20
-
-/* Now we do the RTM bus. This is private to the SRTM Zynq.
- * It has clocks, DDR, firefly (and enable) and VI sensors
- * Symbolic names for each current/voltage sensor.
-*/
-#define VI_P1V8    0x67
-#define VI_P3V3    0x6F
-#define VI_P1V2    0x6B
-#define VI_P0V9    0x6A
-#define VI_P2V5    0x6D
-#define VI_P0V85   0x6C
-#define VI_P3V3FF  0x69 /* For the Fireflies only */
-#define VI_P1V2DDR 0x6E
-
-/* The DDR4 module */
-#define DDR4ADDR 0x51
-
-/* The Zynq internal. No address actually needed */
-#define ZYNQADDR 0xFF
-
-/* ---- Now the sensors on the (optional) tester card -------------------------
- */
-#define VI_12V_BRICK  0x67 /* U8 - 12V brick */
-
-/*
- * This is used only to provide a block of storage to avoid memory fragmenting.
- * The information in this is accessible via the sensorRecord array below.
- */
-static struct sensorReadings {
-  struct tmp100 temperature[NTEMP];
-  struct ltc2945 power[NPOWER];
-  struct pca8575 status[NSTATUS];
-  struct firefly12 txrx12[NFIREFLY-2];
-  struct firefly4 txrx4[2];
-#ifdef ACCESS_SRTM_TESTER
-  struct atca48V brick;
-#endif
-  struct ddr4 ddr4;
-  struct zynqOnBoard zynqInternal;
-} sensorData;
-
-/*
- * Here is the array of structures for the actual sensors on the carrier.
- * It is used by both the I2C direct sensor access and by the IPMC indirect
- * sensor access. For each sensor it contains the following information:
- *    1) The I2C address info
- *    2) The IPMC buffer offset information (NOT USED ON SRTM. BACKWARD COMPAT}
- *    3) Destination for the formatted and raw data. Always in the sensorData
- * Finally, in the I2C block are two fields which have meaning only for the
- * case with the IPMC: Slot no. and IP address. Access functions are provided
- * for these below.
- *
- * Note that the bus mask macro assumes mux buses are numbered starting at 1 ala LArC.
- * in the 3rd field of the I2C address...
- */
-
-#define SENSOR0 0
-#define SENSOR1 1
-
-static int nsensors=NTEMP+NPOWER+NSTATUS+NFIREFLY+NPSEQ+NDDR4+NZYNQ;
-//static int nsensors=NZYNQ;
-static struct sensorRecord sensors[] = {
-  /* ECC - changing {-1,0,0} to {1,0,0} since it causes compiler errors */
-  /* The NTEMP temperature sensors. In the IPMC buffer, 16(byte) = 8(word) has FPGA temp. Skip it */
-  {{SENSOR0,0,0,TMP100A,tmp100Init,tmp100Read,tmp100Format,0},{1,0,0},&sensorData.temperature[0],"SRTM Temp"},
-  /* And the power sensors */
-  {{SENSOR0,0,0,TMPZYNQ, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_zynq_diode},{1,0,0},&sensorData.power[0],"Zynq Diode"},
-  {{SENSOR1,0,0,VI_P1V8, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[1],"SRTM1.8V (U12)"},
-  {{SENSOR1,0,0,VI_P3V3, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[2],"SRTM3.3V (U11)"},
-  {{SENSOR1,0,0,VI_P2V5, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[3],"SRTM2.5V (U12)"},
-  {{SENSOR1,0,0,VI_P1V2, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4630},{1,0,0},&sensorData.power[4],"SRTM1.2V (U1)"},
-  {{SENSOR1,0,0,VI_P0V9, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[5],"SRTM0.9V (U9)"},
-  {{SENSOR1,0,0,VI_P0V85,ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4630},{1,0,0},&sensorData.power[6],"SRTM0.85V (U1)"},
-  {{SENSOR1,0,0,VI_P1V2DDR, ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[7],"SRTM DDR4 1.2V (U9)"},
-  {{SENSOR1,0,0,VI_P3V3FF,ltc2945Init,ltc2945Read,ltc2945Format,(void* (*)())c_ltm4628},{1,0,0},&sensorData.power[8],"SRTM Firefly 3.3V (U11)"},
-  {{SENSOR0,0,0,PSTATUS,pca8575Init,pca8575Read,pca8575Format,0},{1,0,0},&sensorData.status[0],"SRTM Power Status"},
-
-  {{SENSOR1,FFOutputReg,0x80 | 0, FF25TXONLY, fireflyInit12,fireflyRead12,fireflyFormat12,0},{1,0,0},&sensorData.txrx12[0],"FF-TX4"},
-  {{SENSOR1,FFOutputReg,0x80 | 2, FF25TXONLY, fireflyInit12,fireflyRead12,fireflyFormat12,0},{1,0,0},&sensorData.txrx12[1],"FF-TX3"},
-  {{SENSOR1,FFOutputReg,0x80 | 4, FF25TXONLY, fireflyInit12,fireflyRead12,fireflyFormat12,0},{1,0,0},&sensorData.txrx12[2],"FF-TX2"},
-  {{SENSOR1,FFOutputReg,0x80 | 6, FF25TXONLY, fireflyInit12,fireflyRead12,fireflyFormat12,0},{1,0,0},&sensorData.txrx12[3],"FF-TX1"},
-  {{SENSOR1,FFOutputReg,0x80 | 12, FF10TXRX, fireflyInit4,fireflyRead4,fireflyFormat4,0},{1,0,0},&sensorData.txrx4[0],"FF-TXRX12"},
-  {{SENSOR1,FFOutputReg,0x80 | 14, FF10TXRX, fireflyInit4,fireflyRead4,fireflyFormat4,0},{1,0,0},&sensorData.txrx4[1],"FF-TXRX11"},
-  {{SENSOR1,0,0,DDR4ADDR,ddr4Init,ddr4Read,ddr4Format,0},{1,0,0},&sensorData.ddr4,"DDR4"},
-  {{0,0,0,ZYNQADDR,zynqOnBoardInit,zynqOnBoardRead,zynqOnBoardFormat,0},{1,0,0},&sensorData.zynqInternal,"ZynqInternal"},
-  // ECC - The following line was used when we were only looking at zynq data
-  //{{0,0,0,ZYNQADDR,zynqOnBoardInit,zynqOnBoardRead,zynqOnBoardFormat,0},{1,0,0},&sensorData.zynqInternal,"ZynqInternal"},
-  {{0,0,0,0,0,0,0,0},{0,0,0},0,0}
-};
-
-/* Indices into the sensors record above */
-//static enum ffID {FF1=14, FF2=13, FF3=12, FF4=11, FF12=15, FF11=16};
-
-/* ------------------------------------------------------------------------  */
-/* ----  Here is the bus interface definition -----------------------------  */
-/* ------------------------------------------------------------------------  */
-
-#ifndef EXTERNAL_I2C
-static struct i2cBus srtmBuses[] = {
-  {I2COpenDevice,I2CCloseDevice,I2CRead,I2CWrite,I2CMuxEnable,I2CRegEnable,0,0,-1,"/dev/i2c-0"},
-  {I2COpenDevice,I2CCloseDevice,I2CRead,I2CWrite,I2CMuxEnable,I2CRegEnable,0,0,-1,"/dev/i2c-1"}
-};
-#else
-/* This is from a e.g. BeagleBone with eveything on bus 2. Put in a double entry to avoid needing
- * to reconfigure the sensors[] array above.
- */
-static struct i2cBus srtmBuses[] = {
-  {I2COpenDevice,I2CCloseDevice,I2CRead,I2CWrite,I2CMuxEnable,I2CRegEnable,0,0,-1,"/dev/i2c-2"},
-  {I2COpenDevice,I2CCloseDevice,I2CRead,I2CWrite,I2CMuxEnable,I2CRegEnable,0,0,-1,"/dev/i2c-2"},
-};
-#endif
 
 
 namespace Device
@@ -247,81 +107,6 @@ DRegs::~DRegs ()
 {
 }
 
-void fcEnable(int aFirefly) {
-  /* Just get the address record and call the utility routine */
-  struct sensorI2CAddress *ffsa = &(sensors[aFirefly].saI2C);
-  int iret=fireflyEnableI2C(ffsa);
-}
-
-void fcDisable(int aFirefly) {
-  /* Just get the address record and call the utility routine */
-  struct sensorI2CAddress *ffsa = &(sensors[aFirefly].saI2C);
-  int iret=fireflyDisableI2C(ffsa);
-}
-
-#define TXDIS4 86
-#define TXDIS12 52
-
-int fcChanEnable(int aFirefly, int ichan) {
-  fcEnable(aFirefly);
-  printf("In %s, %s with ffID = %d, ichan =%d\n",__FILE__,__FUNCTION__,aFirefly,ichan);
-  struct sensorI2CAddress *ffsa = &(sensors[aFirefly].saI2C);
-  /* The enable register format depends on which version it is. */
-  if( ffsa->initfcn == fireflyInit4 ) { /* 4 channel Tx/Rx module */ 
-	  u8 *data = sensorRead(ffsa,TXDIS4,1);
-	  u8 oldval = data[0];
-	  u8 ibit = 1<<ichan;
-	  u8 mask = ~ibit;
-	  u8 newval[2] = {TXDIS4, oldval & mask};  /* clear the disable bit for this channel */
-	  sensorWrite(ffsa,newval,2);
-	  data = sensorRead(ffsa,TXDIS4,1);
-	  printf("    initial tx disable register = 0x%02x; after enabling channel %d, = 0x%02x\n",oldval,ichan,data[0]);
-  }
-  else { /* 12 channel Tx module */
-	  u8 *data = sensorRead(ffsa,TXDIS12,2);
-	  u16 oldval = (( data[0] << 8 ) & 0xFF00 ) | data[1];
-	  u16 ibit = 1<<ichan;
-	  u16 mask = ~ibit;
-    u16 mval = oldval & mask;
-	  u8 newval[3] = {TXDIS12, ((mval>>8) & 0xFF) , mval & 0xFF};  /* clear the disable bit for this channel */
-	  sensorWrite(ffsa,newval,3);
-	  data = sensorRead(ffsa,TXDIS12,2);
-    mval =  (( data[0] << 8 ) & 0xFF00 ) | data[1];
-	  printf("    initial tx disable register = 0x%03x; after enabling channel %d, = 0x%03x\n",oldval,ichan,mval);
-  }
-  fcDisable(aFirefly);
-  return 0;
-}
-
-int fcChanDisable(int aFirefly, int ichan) {
-  fcEnable(aFirefly);
-  printf("In %s, %s with ffID = %d, ichan =%d\n",__FILE__,__FUNCTION__,aFirefly,ichan);
-  struct sensorI2CAddress *ffsa = &(sensors[aFirefly].saI2C);
-  /* The enable register format depends on which version it is. */
-  if( ffsa->initfcn == fireflyInit4 ) { /* 4 channel Tx/Rx module */ 
-	  u8 *data = sensorRead(ffsa,TXDIS4,1);
-	  u8 oldval = data[0];
-	  u8 ibit = 1<<ichan;
-	  u8 newval[2] = {TXDIS4, oldval | ibit}; /* Set the register address and then the disable bit for this channel */
-	  sensorWrite(ffsa,newval,2);
-	  data = sensorRead(ffsa,TXDIS4,1);
-	  printf("    initial tx disable register = 0x%02x; after disabling channel %d, = 0x%02x\n",oldval,ichan,data[0]);
-  }
-  else { /* 12 channel Tx module */
-	  u8 *data = sensorRead(ffsa,TXDIS12,2);
-	  u16 oldval = (( data[0] << 8 ) & 0xFF00 ) | data[1];
-	  u16 ibit = 1<<ichan;
-    u16 mval = oldval | ibit;
-	  u8 newval[3] = {TXDIS12, ((mval>>8) & 0xFF) , mval & 0xFF};  /* clear the disable bit for this channel */
-	  sensorWrite(ffsa,newval,3);
-	  data = sensorRead(ffsa,TXDIS12,2);
-    mval =  (( data[0] << 8 ) & 0xFF00 ) | data[1];
-	  printf("    initial tx disable register = 0x%03x; after disabling channel %d, = 0x%03x\n",oldval,ichan,mval);
-  }
-  fcDisable(aFirefly);
-  return 0;
-}
-
 
 /* delegates for cachevariables */
 
@@ -329,7 +114,6 @@ int fcChanDisable(int aFirefly, int ichan) {
 void DRegs::update() {
 
   static int maxprint = 0;
-  OpcUa_UInt32 val = 0;
   OpcUa_UInt32 spi[] = {
     0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0,
@@ -338,7 +122,6 @@ void DRegs::update() {
     0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0,
     0, 0};
-  static int nspi = 38;
 
   static int first = 1;
 
@@ -348,7 +131,6 @@ void DRegs::update() {
   // This is the physical base address which is set by the constructor
   off_t bram_pbase = address();
   OpcUa_UInt32 *bram32_vptr;
-  int fd;
 
   // see if we want to write to this address
   OpcUa_UInt32 setval = 0;
@@ -380,24 +162,28 @@ void DRegs::update() {
   fprintf(json_file, cJSON_Print(top));
   fclose(json_file);
 
-  // Print out i2c values using JSON 
-  //printf("%s\n",cJSON_Print(top));
+  int tot_sensors = sensorCount();
 
 
-  /*
+  // print out the sensor data
   if (maxprint < 3) {
-    std::cout << std::endl << "iteration: " << maxprint << std::endl;
-    for (int iv=0; iv<35; iv++) std::cout << "zynq vals (" << iv << "): " << names[iv] << " " << vals[iv] << std::endl;
-
-    // Print out firefly values here
-    std::cout << std::endl;
-    for (int iv=0; iv<38; iv++) {
-      std::cout << "FF vals: " << ff_names[iv] << " " << ff_vals[iv] << std::endl;
+    std::cout << "Number of sensors: " << tot_sensors << std::endl;
+    for (int i=0; i<tot_sensors; i++) {
+      const struct sensorRecord *sensor_data;
+      sensor_data = sensorGet(i);
+      std::cout << "i: " << i << " name: " << sensor_data->name;
+      void *buffer = sensor_data->valueBuffer;
+      if (i<8) {
+	float val1 = *static_cast<float *>(buffer);
+	std::cout << " val1: " << val1 << std::endl;
+      }
+      else std::cout << std::endl;
     }
   }
-  */
 
   maxprint++;
+
+  // For now just do the first 8 sensors...
 
   /******
   // Push the values to the OpcUa client display
